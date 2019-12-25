79a2e3d449955877aae9cf4758a55904bce18583
Alfe Caesar Lagas <alfecaesar@gmail.com>
2019-12-26 00:10:38 +0800

    Updates

---
+++ precache-manifest.168f6ffd24f07738c473ae4650b270a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "514c0f959aa3ba292ae937785aa65f70",
+    "url": "/reactjs-portfolio/index.html"
+  },
+  {
+    "revision": "a8234e36db6e3dd35b9c",
+    "url": "/reactjs-portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5a0271837b419002a879",
+    "url": "/reactjs-portfolio/static/js/2.483b45e6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/reactjs-portfolio/static/js/2.483b45e6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8234e36db6e3dd35b9c",
+    "url": "/reactjs-portfolio/static/js/main.89cc3c0d.chunk.js"
+  },
+  {
+    "revision": "d8b268a98fc69bf79fc3",
+    "url": "/reactjs-portfolio/static/js/runtime-main.7527044b.js"
+  }
+]);
